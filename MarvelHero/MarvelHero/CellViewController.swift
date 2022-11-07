5d61f7659dcd9f444f43880f258790b735cef40e
Annitas <deadfoxmaster@mail.ru>
2022-11-07 06:36:12 +0600

    remove storyboard

---
+++ MarvelHero/MarvelHero/CellViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelHero
+//
+//  Created by Anita Stashevskaya on 07.11.2022.
+//
+
+import UIKit
+
+class CellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
