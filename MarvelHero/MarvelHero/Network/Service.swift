42d62c03facdfdd41b26f51e11a5571dc700f4af
Annitas <deadfoxmaster@mail.ru>
2022-11-10 17:23:02 +0600

    get marvel API request

--- MarvelHero/MarvelHero/Network/Service.swift
+++ MarvelHero/MarvelHero/Network/Service.swift
@@ -12,19 +12,51 @@ final class Service {
     private let privateKey = "ed7b59c70915033e0e20f96eb71c36ae3e8ee521"
     private let publicKey = "4a1ae51af1869016810ae8f924e935a0"
 
+    
+    private let baseUrl = "https://gateway.marvel.com/v1/public/"
+    
+    func endpoint(path: String) -> String {
+        return baseUrl + path
+    }
     //MARK: get some data from marvel api
+//    func getSmthFromInternet() {
+//
+//        AF.request(
+//            endpoint(path: "characters"),
+//            method: .get,
+//            parameters: getParams(),
+//            encoder: URLEncodedFormParameterEncoder(destination: .queryString)
+//        ).response { (responseData) in
+//            guard let data = responseData.data else { return }
+//            do {
+//                let heroes = try JSONDecoder().decode(CharacterDataWrapper.self, from: data)
+//                print("heroes == \(heroes)")
+//            } catch {
+//                print("Error decoding == \(error)")
+////            }
+//        }
+//    }
+    
     func getSmthFromInternet() {
         let ts = String(Date().timeIntervalSince1970)
         let hash = MD5(data: "\(ts)\(privateKey)\(publicKey)")
-        let requestURL = "http://gateway.marvel.com:443/v1/public/characters?ts=\(ts)&apikey=\(publicKey)&hash=\(hash)"
-        AF.request(requestURL, method: .get, parameters: nil, encoding: URLEncoding.default,
-                   headers: nil, interceptor: nil).response { (responseData) in
-            guard let data = responseData.data else { return }
-            do {
-                let heroes = try JSONDecoder().decode(Hero.self, from: data)
-                print("heroes == \(heroes)")
-            } catch {
-                print("Error decoding == \(error)")
+        let url = "https://gateway.marvel.com/v1/public/characters?orderBy=name&ts=\(ts)&apikey=\(publicKey)&hash=\(hash)"
+        
+        AF.request(url, method: .get,
+                   parameters: nil,
+                   encoding: URLEncoding.default,
+                   headers: nil,
+                   interceptor: nil).response { responce in
+            switch responce.result {
+            case .success(let data):
+                do {
+                    let jsondata = try JSONDecoder().decode(CharacterDataWrapper.self, from: data!)
+                    print(jsondata)
+                } catch {
+                    
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
             }
         }
     }
@@ -37,4 +69,16 @@ final class Service {
         .joined()
     }
 
+    private func getParams() -> Params {
+        let ts = String(Date().timeIntervalSince1970)
+        let hash = MD5(data: "\(ts)\(privateKey)\(publicKey)")
+        
+        return Params(apiKey: publicKey, hash: hash)
+    }
+}
+
+struct Params: Encodable {
+    let apiKey: String
+    let hash: String
+    
 }
